2c9176f808fa3ef8727fe021a30abe99a4fa10b6
Mert Karagul <gmert1702@gmail.com>
2023-11-15 00:07:27 +0300

    DotRestaurant setup to Git

---
+++ DotRestaurant/Utils/Constans.cs
@@ -0,0 +1,8 @@
+﻿namespace DotRestaurant.Utils
+{
+    public class Constans
+    {
+        public static string RESTURANT_NAME = "DotRestaurant";
+        public static string RESTURANT_SLOGAN = "DotRestaurant olarak geniş menümüz ve hizmetimiz ile sizlere leziz yiyecekler sunuyoruz.";
+    }
+}
